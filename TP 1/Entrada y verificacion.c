48bbe45d409f864c6c468fa69c1ae435033533d5
LMFT <lucasferrini1607@gmail.com>
2020-09-08 17:21:48 -0300

    Menu Corregido, agregadas nuevas funciones

--- TP 1/Entrada y verificacion.c
+++ TP 1/Entrada y verificacion.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include "Entrada y verificacion.h"
 
@@ -45,40 +45,3 @@ void cargarNumerosArray(int numeros[], int cant)
 
     }
 }
-
-=======
-#include <stdio.h>
-
-/** \brief Recibe un dato ingresado por el usuario, valida que sea un numero flotante y retorna su valor
- *
- * \return float El numero ingresado ya validado
- *
- */
-float pedirNumero()
-{
-    float numero;
-    while(!(scanf("%f", &numero) == 1))
-    {
-        fflush(stdin);
-        printf("\nEl valor ingresado no es un numero. Ingrese un numero: ");
-    }
-
-
-
-    return numero;
-}
-
-char pedirCaracter()
-{
-    char caracter;
-    while(!(scanf("%c", &caracter) == 1))
-    {
-        fflush(stdin);
-        printf("\nEl valor ingresado no es un caracter. Ingrese un caracter: ");
-    }
-
-
-
-    return caracter;
-}
->>>>>>> 315ed78b7b96f3de95966b4aef8b5764167d733a
